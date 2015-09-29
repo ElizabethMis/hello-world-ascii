87f35fe761e2bb78e53831948c093683bb51587c
Elizabeth Mis <elizabethmis@Elizabeths-MacBook-Pro.local>
2015-09-28 21:34:11 -0400

    First Commit

---
+++ HelloWorldAscii.java
@@ -0,0 +1,15 @@
+public class HelloWorldAscii {
+	public static void main (String[] args){
+	
+		System.out.println("  _______      ----  ----     _______                            __     __                __ ");
+		System.out.println(" |_     _|    /    \\/    \\   |    ___|.----..-----..-----..----.|  |--.|__|.-----..-----.|  |");
+		System.out.println("   |   |_     \\          /   |    ___||   _||  -__||     ||  __||     ||  ||  -__||__ --||__|");
+		System.out.println(" |_______|     \\        /    |___|    |__|  |_____||__|__||____||__|__||__||_____||_____||__|");
+		System.out.println("                \\      /");
+		System.out.println("                 \\    /");
+		System.out.println("                  \\  /");
+			
+	}
+	
+}
+	
